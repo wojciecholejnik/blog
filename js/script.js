643edd0a03d1f5a8867327b88ae118d7ef2ea5fa
Wojciech Olejnik <wojciecholejnik5@gmail.com>
2020-08-07 18:28:17 +0200

    Finish Event Handler works fine

--- js/script.js
+++ js/script.js
@@ -5,28 +5,37 @@
     }); */
 
     const titleClickHandler = function (event) {
+        event.preventDefault();
+        const clickedElement = this;
         console.log('Link was clicked!');
         console.log(event);
 
-        /* remove class 'active' from all article links  */
+
+        /* [DONE] remove class 'active' from all article links  */
         const activeLinks = document.querySelectorAll('.titles a.active');
 
         for (let activeLink of activeLinks) {
             activeLink.classList.remove('active');
         }
-        /* add class 'active' to the clicked link */
+        /* [DONE] add class 'active' to the clicked link */
+        clickedElement.classList.add('active');
+        console.log('dodano active do :' + clickedElement);
 
-        /* remove class 'active' from all articles */
+
+        /* [DONE] remove class 'active' from all articles */
         const activeArticles = document.querySelectorAll('article.active');
 
         for (let activeArticle of activeArticles) {
             activeArticle.classList.remove('active');
         }
-        /* get 'href' attribute from the clicked link */
+        /* [DONE] get 'href' attribute from the clicked link */
+        let hrefAttribute = clickedElement.getAttribute('href')
 
-        /* find the correct article using the selector (value of 'href' attribute) */
-
+        /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+        let findedArticle = document.querySelector(hrefAttribute);
+        console.log('findedArticle :' + findedArticle);
         /* add class 'active' to the correct article */
+        findedArticle.classList.add('active');
     }
 
     const links = document.querySelectorAll('.titles a');
