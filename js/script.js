cb7b287c8c8d4444b145fd16afb05a00b5bf97b6
Wojciech Olejnik <wojciecholejnik5@gmail.com>
2020-08-16 13:43:13 +0200

    HELP

--- js/script.js
+++ js/script.js
@@ -104,11 +104,55 @@
                 /* add generated code to html variable */
                 findedWrapper.insertAdjacentHTML('beforeend', html);
             /* END LOOP: for each tag */
-            } console.log(findedWrapper);
+            } //console.log(findedWrapper);
             /* insert HTML of all the links into the tags wrapper */
 
         /* END LOOP: for every article: */
         }
     };
     generateTags();
+
+    const tagClickHandler = function(event){
+        /* prevent default action for this event */
+        event.preventDefault();
+
+        /* make new constant named "clickedElement" and give it the value of "this" */
+        const clickedElement = this;
+
+        /* make a new constant "href" and read the attribute "href" of the clicked element */
+        const href = clickedElement.getAttribute('href');
+        console.log(href);
+
+        /* make a new constant "tag" and extract tag from the "href" constant */
+        const tag = href.replace('#tag-', '');
+        /* find all tag links with class active */
+        findedTags = document.querySelectorAll('a[href="' + href + '"]');
+        /* START LOOP: for each active tag link */
+        for (let findedTag of findedTags){
+
+        /* remove class active */
+
+        /* END LOOP: for each active tag link */
+      }
+        /* find all tag links with "href" attribute equal to the "href" constant */
+
+        /* START LOOP: for each found tag link */
+
+        /* add class active */
+
+        /* END LOOP: for each found tag link */
+
+        /* execute function "generateTitleLinks" with article selector as argument */
+        //generateTitleLinks('[data-tags~="' + tag + '"]');
+    };
+    const addClickListenersToTags = function(){
+        /* find all links to tags */
+
+        /* START LOOP: for each link */
+
+        /* add tagClickHandler as event listener for that link */
+
+        /* END LOOP: for each link */
+    };
+
 }
